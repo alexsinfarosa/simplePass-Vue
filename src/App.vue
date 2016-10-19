0cee67e1bf8c2e5cc790f7571a6f3936eb82e8dd
alexsinfarosa <as898@cornell.edu>
2016-10-19 20:32:44 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,257 @@
+<template>
+<div id="app">
+  <section class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+
+        <!-- Header -->
+        <div class="columns">
+          <div class="column is-8 is-offset-2">
+            <h1 class="title simple-pass">
+                <strong>Simple</strong>
+                <span class="pass">Pass</span>
+              </h1>
+            <hr/>
+            <br/>
+          </div>
+        </div>
+
+        <div class="columns" v-if="!loggedIn">
+          <div class="column is-8 is-offset-2">
+            <button v-on:click="getLoggedIn" class="button is-success" type="button">LOG IN</button>
+          </div>
+        </div>
+
+        <div v-else v-cloak>
+
+          <div class="columns">
+            <div class="column is-8 is-offset-2">
+              <button v-on:click="getLoggedOut" class="button is-primary" type="button">LOG OUT</button>
+            </div>
+          </div>
+
+          <div class="columns">
+            <div class="column is-8 is-offset-2">
+              <table class="table is-striped">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Username/Email</th>
+                    <th>Notes</th>
+                    <th>Password</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                <tfoot>
+                  <th>Tot. {{accounts.length}}</th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                </tfoot>
+                <tbody>
+
+                  <tr
+                    v-for="(account, index) in filteredAccounts"
+                    :key="index"
+                    :class="{editing: account.editingMode}"
+                  >
+                    <td>{{account.name}}</td>
+                    <td>{{account.usernameEmail}}</td>
+                    <td>{{account.notes}}</td>
+                    <td>******{{account.password.slice(-1)}}</td>
+                    <td class="is-icon">
+                      <a
+                        @click.prevent="editAccount(account, index, account.editingMode = true)"
+                        href="#"
+                      >
+                        <i class="fa fa-edit"></i>
+                      </a>
+                    </td>
+                    <td class="is-icon">
+                      <a @click.prevent="removeAccount(index)" href="#">
+                        <i class="fa fa-remove"></i>
+                      </a>
+                    </td>
+                  </tr>
+
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <div class="columns">
+            <div class="column is-8 is-offset-2">
+              <form v-on:submit.prevent="editing ? updateAccount() : addAccount()">
+                <div class="control is-horizontal">
+                  <input
+                    autofocus autocomplete="off"
+                    class="input"
+                    type="text"
+                    required
+                    placeholder="Name"
+                    v-model="newAccount.name"
+                  />
+
+                  <input
+                    autocomplete="off"
+                    class="input"
+                    type="text"
+                    required
+                    placeholder="Username/Email"
+                    v-model="newAccount.usernameEmail"
+                  />
+
+                  <input
+                    class="input"
+                    type="text"
+                    placeholder="Notes"
+                    v-model="newAccount.notes"
+                  />
+
+                  <input
+                    autocomplete="off"
+                    class="input"
+                    type="password"
+                    required
+                    placeholder="Password"
+                    v-model="newAccount.password"
+                  />
+
+                  <p className="control">
+                    <button type="submit" class="button is-outlined is-success">
+                      {{editing ? 'UPDATE' : 'CREATE'}}
+                    </button>
+                  </p>
+                </div>
+              </form>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+</div>
+</template>
+
+<script>
+const filters = {
+  all (accounts) {
+    return accounts
+  },
+  active (accounts) {
+    return accounts.filter(function (account) {
+      return !account.completed
+    })
+  },
+  editing (accounts) {
+    return accounts.filter(function (account) {
+      return account.editing
+    })
+  }
+}
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      accounts: [],
+      newAccount: {
+        id: null,
+        editingMode: null,
+        name: '',
+        usernameEmail: '',
+        password: '',
+        notes: ''
+      },
+      editing: false,
+      visibility: 'all',
+      loggedIn: false
+    }
+  },
+  computed: {
+    filteredAccounts () {
+      return filters[this.visibility](this.accounts)
+    }
+  },
+  methods: {
+    getLoggedIn () {
+      this.loggedIn = true
+    },
+
+    getLoggedOut () {
+      this.loggedIn = false
+    },
+
+    addAccount () {
+      const name = this.newAccount.name && this.newAccount.name.trim()
+      const usernameEmail = this.newAccount.usernameEmail && this.newAccount.usernameEmail.trim()
+      const password = this.newAccount.password && this.newAccount.password.trim()
+      const notes = this.newAccount.notes && this.newAccount.notes.trim()
+      if (!(name && usernameEmail && password)) {
+        return
+      }
+      this.accounts.push({
+        id: null,
+        editingMode: false,
+        name: name,
+        usernameEmail: usernameEmail,
+        password: password,
+        notes: notes
+      })
+      this.newAccount = {}
+    },
+
+    removeAccount (index) {
+      this.accounts.splice(this.accounts.indexOf(index), 1)
+    },
+
+    editAccount (account, index) {
+      this.editing = true
+      this.newAccount = account
+      const id = index
+      const name = this.newAccount.name && this.newAccount.name.trim()
+      const usernameEmail = this.newAccount.usernameEmail && this.newAccount.usernameEmail.trim()
+      const password = this.newAccount.password && this.newAccount.password.trim()
+      const notes = this.newAccount.notes && this.newAccount.notes.trim()
+      if (!(name && usernameEmail && password)) {
+        return
+      }
+      this.newAccount = {
+        id: id,
+        editingMode: true,
+        name: name,
+        usernameEmail: usernameEmail,
+        password: password,
+        notes: notes
+      }
+    },
+
+    updateAccount () {
+      this.newAccount.editingMode = false
+      this.accounts[this.newAccount.id] = this.newAccount
+      this.editing = false
+      this.newAccount = {}
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+[v-cloak] {
+  display: none;
+}
+
+.simple-pass
+  font-size 50px
+
+.pass
+  color #ff3860
+
+.editing
+  background-color rgba(yellow,.1);
+</style>
